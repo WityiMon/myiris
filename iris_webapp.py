2f7d860430f4e7ce6710ce1388f9e1fef5f643fa
WityiMon <72373806+WityiMon@users.noreply.github.com>
2025-05-22 21:16:35 +0630

    Add files via upload

---
+++ iris_webapp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import joblib
+import numpy as np
+st.set_page_config(page_title = "My Iris Test")
+st.title("Iris Flower Classification")
+##@st.cache(allow_output_mutation= True)
+@st.cache_resource
+def get_model():
+    return joblib.load('iris_model.joblib')
+st.image('iris.jpg',caption='Iris Flower')
+spl = st.text_input('Enter sepal length:','')
+spw = st.text_input('Enter sepal width:','')
+pel = st.text_input('Enter petal length:','')
+pew = st.text_input('Enter petal width:','')
+if st.button('Classify Flower'):
+    values = [spl,spw,pel,pew]
+    num_values = [float(x) for x in values]
+    nm_values_2D = np.asarray(num_values).reshape(1,-1)
+    knn_model = get_model()
+    pred_value = knn_model.predict(nm_values_2D)
+    if pred_value == 0:
+        st.write("this is setosa")
+    elif pred_value == 1:
+        st.write("this is versicolor")
+    else:
+        st.write("this is virginica")
